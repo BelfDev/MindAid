fc2acb5012b5080a70e5d0829313ae63612981ef
BelfDev <belfortdev@gmail.com>
2015-03-26 11:54:26 -0300

    Inicializando

---
+++ MindAid/Activity.swift
@@ -0,0 +1,23 @@
+//
+//  Activity.swift
+//  MindAid
+//
+//  Created by Pedro Henrique Belfort Fernandes on 3/26/15.
+//  Copyright (c) 2015 Pedro Belfort. All rights reserved.
+//
+
+import UIKit
+
+class Activity: NSObject {
+    
+    var title:String = String()
+    var imageString:String = String()
+    
+    //Methods
+    
+    func createImage (imageName:String)->(UIImageView){
+        var image = UIImage(named:imageName)
+        var imageView = UIImageView (image:image)
+        return imageView
+    }
+}
---
+++ MindAid/ActivityVC.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityVC.swift
+//  MindAid
+//
+//  Created by Pedro Henrique Belfort Fernandes on 3/26/15.
+//  Copyright (c) 2015 Pedro Belfort. All rights reserved.
+//
+
+import UIKit
+
+class ActivityVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
