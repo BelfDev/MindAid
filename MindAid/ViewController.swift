fbb74b0d2b9da50da2f610b440d029433b5e9afb
BelfDev <belfortdev@gmail.com>
2015-03-26 11:12:27 -0300

    Initial Commit

---
+++ MindAid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindAid
+//
+//  Created by Pedro Henrique Belfort Fernandes on 3/26/15.
+//  Copyright (c) 2015 Pedro Belfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
